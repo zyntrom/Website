d1c9f7cd58568bf4999c4104eba942184bb2bf61
zyntrom <zyntrom09@gmail.com>
2024-11-10 11:08:35 +0530

    Reorganised login and registration code

--- backend/index.php
+++ backend/index.php
@@ -1,54 +1,86 @@
-<?php 
-	include("database.php");
-	session_start();	
-	$username=$_POST['username'];
-	$password=$_POST['password'];
-	$email=$_POST['email'];
-	$hash=password_hash($password,PASSWORD_DEFAULT);
-	$logusername=$_POST['logusername'];
-	$logpassword=$_POST['logpassword'];
-	echo"test";
-	if(isset($username)){
-		$sql="insert into users (username, email, password) values ('$username','$email','$hash')";
-		
-		try{	
-			mysqli_query($conn,$sql);
-			echo"User is registerd";
-			header('Location: ../frontend/dashboard.html');
-			exit;
-		}
-		catch(mysqli_sql_exception $e){
-			echo"Could not Register";
-		}
-	}
-	elseif(isset($logusername)){
-		$sql="select * from users where username = '$logusername'";
-		try{
-			
-			$result=mysqli_query($conn,$sql);
-		}	
-		catch(mysqli_sql_exception $e){
-			echo"Could not check";
-		}
-		if(mysqli_num_rows($result)>0){
-			$row=mysqli_fetch_assoc($result);
-			if(password_verify($logpassword,$row["password"])){
-				$_SESSION["username"]=$row['username'];
-				$_SESSION['id']=$row['id'];
-				setcookie("session_id", session_id(), time() + 2592000, "/");
-				header("Location: ../frontend/dashboard.html");
-				exit;
-			}
-			else{
-				echo"Wrong password";	
-			}
-		}
-		else{
-			echo"Not found";
-		}
-	}
-	else{
-		echo"Invalid";
-	}
-	mysqli_close($conn);
+<?php
+
+    session_start();
+    //Handles Registation
+    function register(){
+
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+            $regusername=filter_input(INPUT_POST,'username',FILTER_SANITIZE_SPECIAL_CHARS);
+            $regpassword=filter_input(INPUT_POST,'password',FILTER_SANITIZE_SPECIAL_CHARS);
+            $regemail=filter_input(INPUT_POST,'email',FILTER_SANITIZE_SPECIAL_CHARS);
+            $hash=password_hash($regpassword,PASSWORD_DEFAULT);
+            if(isset($username)){   
+                //Add the new user
+                $sql="insert into users (username, email, password) values ('$regusername','$regemail','$hash')";
+                try{	
+                    include("database.php");
+                    mysqli_query($conn,$sql);
+                    echo"User is registerd";
+                    $_SESSION["username"]=$regusername;
+                    setcookie("session_id", session_id(), time() + 2592000, "/");
+                    header('Location: ../frontend/dashboard.html');
+                    exit;
+                }
+                catch(mysqli_sql_exception $e){
+                    echo"Could not Register";
+                }
+            }
+            else{
+                echo "please input username";
+            }
+	    }
+
+    }
+    //handles Login
+    function login(){
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+            $logusername=$_POST['logusername'];
+            $logpassword=$_POST['logpassword'];
+            if(isset($logusername)){
+                //Gets the user from the database
+                $sql="select * from users where username = '$logusername'";
+                try{
+                    include("database.php");
+                    $result=mysqli_query($conn,$sql);
+                }	
+                catch(mysqli_sql_exception $e){
+                    echo"Could not check";
+                }
+                //Confirms Password
+                if(mysqli_num_rows($result)>0){
+                    $row=mysqli_fetch_assoc($result);
+                    if(password_verify($logpassword,$row["password"])){
+                        $_SESSION["username"]=$row['username'];
+                        $_SESSION['id']=$row['id'];
+                        setcookie("session_id", session_id(), time() + 2592000, "/");
+                        echo "Welcome";
+                        header("Location: ../frontend/dashboard.html");
+                        exit;
+                    }
+                    else{
+                        echo"Wrong password";	
+                    }
+                }
+                else{
+                    echo"User Not found";
+                }
+            }
+        }
+    }
+
+    //Checks whether login or register button is pressed
+    if(isset($_POST['register'])){
+        register();
+    }
+    elseif(isset($_POST['login'])){
+        login();
+    }
+    else{
+        exit;
+    }
+    
+
+    mysqli_close($conn);
+
+
 ?>
